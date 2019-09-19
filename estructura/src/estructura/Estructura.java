56a5c8a38a0ea35530d6b3604fbf45b217e41124
USUARIO <USUARIO@PC5>
2019-09-19 10:33:40 -0500

    switche retrochimba

--- estructura/src/estructura/Estructura.java
+++ estructura/src/estructura/Estructura.java
@@ -1,11 +1,30 @@
 
 package estructura;
 
+import java.util.Scanner;
+
 public class Estructura {
 
    
     public static void main(String[] args) {
-        System.out.println("hola muuuudo");
-      
+        Scanner sc =new Scanner(System.in);
+        
+        System.out.println(" 1.para reirse"+" 2.para hablar"+" 3.para depedirese");
+        int opcion = sc.nextInt();
+        
+            switch(opcion){
+                
+                case 1: 
+                    System.out.println("JAJAJAJAJAJAJAJAJ");
+                    break;
+                    
+                    case 2 : System.out.println("hola como estas?");
+                    break;
+                
+                    case 3 : System.out.println("byeee");
+                    break;
+                
+            }
+       
     }
 }
